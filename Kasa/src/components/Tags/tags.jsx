311ab1ea561ab4d01abffe9aca62691e836d21b0
Louis.LB <145657165+LouisLB40@users.noreply.github.com>
2024-04-01 14:41:52 +0200

    Tags completed

---
+++ Kasa/src/components/Tags/tags.jsx
@@ -0,0 +1,12 @@
+export function Tags({ tags }) {
+  return (
+    tags &&
+    Array.isArray(tags) && (
+      <ul className="logements-container_tags">
+        {tags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))}
+      </ul>
+    )
+  );
+}
